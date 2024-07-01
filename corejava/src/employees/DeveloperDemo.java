ef5516f9c9746227b12690666d0fe89745ccd4b5
win10 <win10@DESKTOP-18I6LMR>
2024-07-01 13:48:00 +0530

    first commit

---
+++ corejava/src/employees/DeveloperDemo.java
@@ -0,0 +1,15 @@
+package employees;
+
+public class DeveloperDemo {
+	public static void main(String args[])
+	{
+		Developer obj=new Developer();
+		
+		obj.setName("Shruthi");
+		obj.setDeveloperid(114);
+		obj.setSalary(200000);
+		
+		System.out.println(obj);
+}
+
+}
